d88a89fb5b60d9f00025f58db9446c328d4d504c
AlvaroAzisi <anomaly.anomaly.exe@gmail.com>
2026-08-31 10:05:04 +0700

    chore: remove stray debug files and duplicate config

--- testBalinese.js
+++
@@ -1,8 +0,0 @@
-import { toBalinese } from "./src/lib/aksaraConverter.js";
-const word = "katam";
-const out = toBalinese(word);
-console.log("Output:", out);
-console.log(
-  "Codepoints:",
-  [...out].map((c) => c.codePointAt(0).toString(16).toUpperCase()).join(" "),
-);
